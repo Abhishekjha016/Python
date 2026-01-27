6db2ee25af39be08125a47a508701609f0cb59fd
Abhishekjha016 <abhishekjha01611@gmail.com>
2026-01-28 01:32:01 +0530

    Final commit

--- Module-1/Task-2.py
+++ Module-1/Task-2.py
@@ -1,3 +1,7 @@
 #Personalised Greeting(Task 2)
-a1=input("Enter your first name: ")
-b1=input("Enter your last name: ")
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+full_name= first_name+" " +last_name
+Final_output="hey"+", "+full_name+"! "+"Welcome to python program."
+print(Final_output)
