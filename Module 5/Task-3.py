28185a63bd581085778381582c0fe345c34226bd
Abhishekjha016 <abhishekjha01611@gmail.com>
2026-01-31 13:20:24 +0530

    2nd assignment

---
+++ Module 5/Task-3.py
@@ -0,0 +1,6 @@
+#Problem : Write a program to check if a no. is even or odd.
+num_1 = int(input("Enter the Number: "))
+if num_1 % 2 == 0:
+     print(f"{num_1} is an even number.")
+else:
+     print(f"{num_1} is an odd number.")
